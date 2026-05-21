e6aee1e2abc9c2fdd187b649273cc8f5e77f533e
nadia-bikbaeva <stud0000295927@study.utmn.ru>
2026-05-22 02:10:55 +0500

    php tasks

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="src/styles/styles.css">
 </head>
 <body>
-<h1>Практика 17 :з</h1>
+<h1>Практика 17 >:)</h1>
 <ul>
     <li><a href="task1.php">Задание 1</a></li>
     <li><a href="task2.php">Задание 2</a></li>
